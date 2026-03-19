7ccda8e209bc44250a3fae6cfed9706608158a7e
Jair Poveda <jair@cooltimedia.com>
2026-03-19 16:23:20 -0500

    updates

--- app/settings/production.py
+++ app/settings/production.py
@@ -15,13 +15,13 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 STATICFILES_DIRS = [
-    PROJECT_DIR / "static",
+    BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = PROJECT_DIR / "static"
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = PROJECT_DIR / "media"
 MEDIA_URL = "/media/"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
